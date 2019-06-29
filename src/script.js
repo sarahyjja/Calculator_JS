3bb33b723452edb26c80181dc2fa86c597fe424f
sarah <sarahkharraz@gmail.com>
2019-06-29 19:50:06 +0100

    Create logic for eval function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,55 @@
+let expression = "";
+
+function appendValue(value) {
+  expression += value
+  document.getElementById("display").value = expression;
+}
+
+function calculate() {
+    const [number1, operator, number2] = parseExpression(expression)
+    const result = evalOperation(number1, number2, operator);
+    document.getElementById("display").value = expression = result;
+}
+
+function parseExpression(expression) {
+    // I.e. "5*7" -> [5, "*", 7]
+    if(expression.includes("+")) {
+        return parseForOperator(expression, "+")
+    }
+    
+    if(expression.includes("-")) {
+        return parseForOperator(expression, "-")
+    }
+    
+    if(expression.includes("*")) {
+        return parseForOperator(expression, "*")
+    }
+    
+    if(expression.includes("/")) {
+        return parseForOperator(expression, "/")
+    }
+}
+
+function parseForOperator(expression, operator) {
+    const expressionList = expression.split(operator)
+    let number1 = parseInt(expressionList[0])
+    const number2 = parseInt(expressionList[1])
+    return [number1, operator, number2] 
+}
+
+function evalOperation(number1, number2, operation) {
+    switch(operation) {
+        case "+": return add(number1, number2)
+        case "-": return substract(number1, number2)
+        case "*": return multiply(number1, number2)
+        case "/": return divide(number1, number2)
+    }
+}
+
+function cleanDisplay() {
+    document.getElementById("display").value = expression = "";
+}
+
 function add(a, b) {
   return a + b
 }
@@ -12,21 +64,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b
-}
-
-let expression = "";
-
-function appendValue(value) {
-  expression += value
-  document.getElementById("display").value = expression;
-}
-
-function calculate() {
-  const result = expression = eval(expression)
-  document.getElementById("display").value = result;
-}
-
-function cleanDisplay() {
-    console.log("sadsa")
-    document.getElementById("display").value = expression = "";
 }
