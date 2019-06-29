d967c3da35ba87646fa202547591e39ff37b105a
sarah <sarahkharraz@gmail.com>
2019-06-29 19:13:37 +0100

    Extract logic to JS file

--- src/script.js
+++ src/script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+let expression = "";
+
+function appendValue(value) {
+  expression += value
+  document.getElementById("display").value = expression;
+}
+
+function calculate() {
+  const result = expression = eval(expression)
+  document.getElementById("display").value = result;
+}
+
+function cleanDisplay() {
+    console.log("sadsa")
+    document.getElementById("display").value = expression = "";
+}
